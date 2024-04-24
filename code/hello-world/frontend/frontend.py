cacd7677a35872c9319e51f9dc71008c21bdf2b8
franznkemaka <franz@rigle.co>
2024-04-24 14:45:50 +0200

    🔧

--- code/hello-world/frontend/frontend.py
+++ code/hello-world/frontend/frontend.py
@@ -3,12 +3,13 @@ import socket
 import os
 import requests
 from datetime import datetime
-
+from dotenv import load_dotenv
 
 hostname = os.uname()[1]
 ips = socket.gethostbyname_ex(hostname)[2]
 
 
+load_dotenv()
 app = Flask(__name__)
 
 
@@ -25,10 +26,12 @@ def index():
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         file.write(f"\n Processed by {ips} at {now}")
 
+    secret_pass = os.getenv("DB_SECRET")
+
     backend_response = requests.get("http://backend:80").text
     backend_response = "Backend is coming later!"
     backend_response = ips
-    return f'<html style="background:aqua;">Hello World V3, The backend said: {backend_response} </html>'
+    return f'<html style="background:aqua;">Hello World V3, DB_SECRET: {secret_pass},  The backend said: {backend_response} </html>'
     # return f'<html style="background:orange;">Hello World V1 </html>'
 
 
