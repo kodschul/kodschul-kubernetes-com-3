94bc3795cf976d4c59e9d3d73ff16b43a7b3001e
franznkemaka <franz@rigle.co>
2024-04-24 12:08:14 +0200

    🔧 dynamic file creation + volumes

--- code/hello-world/frontend/frontend.py
+++ code/hello-world/frontend/frontend.py
@@ -2,6 +2,7 @@ from flask import Flask
 import socket
 import os
 import requests
+from datetime import datetime
 
 
 hostname = os.uname()[1]
@@ -14,10 +15,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
+    try:
+        os.mkdir("/data")
+    except:
+        pass
+
+    with open("/data/access.log", "a+") as file:
+
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file.write(f"\n Processed by {ips} at {now}")
+
     backend_response = requests.get("http://backend:80").text
     backend_response = "Backend is coming later!"
     backend_response = ips
-    return f'<html style="background:green;">Hello World V2, The backend said: {backend_response} </html>'
+    return f'<html style="background:aqua;">Hello World V3, The backend said: {backend_response} </html>'
     # return f'<html style="background:orange;">Hello World V1 </html>'
 
 
